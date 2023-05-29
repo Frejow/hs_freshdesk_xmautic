fb6d14739440f340f76374cd206a50e3a9d78452
Jow Doe <frejusbessan08@yahoo.com>
2023-05-29 16:30:23 +0100

    replace file_put_contents by error_log

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $files = file_get_contents('php://input');
 try {
 
     $jsonData = json_decode($files);
-    file_put_contents(getcwd().'/log.txt', print_r($jsonData, true));
+    error_log(print_r($jsonData, true));
     
 }
 catch(Exception $e) {
